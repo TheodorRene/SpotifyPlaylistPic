7450df63ac6a59ebcbc1106c0d24e44c495171eb
Theodor René Carlsen <theodor.rene@gmail.com>
2018-12-07 22:39:32 +0100

    inital commit. add support for constrast and simple text

---
+++ script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+img=$1
+text=$2
+
+cp $img "PlaylistImage$img"
+newImg="PlaylistImage$img"
+
+
+#Adding constrast
+resolution=$(identify $newImg | awk '{print $4}')
+#echo $resolution
+
+echo $newimg 
+convert $newImg -level 2%,98%,0.5 \
+        -crop '640x640+5+10' \
+        $newImg 
+
+#echo $(identify $newImg | awk '{print $4}')
+
+echo $newimg
+
+convert $newImg \
+        -gravity Center \
+        -pointsize 72 \
+        -fill white \
+        -font Proxima-Nova-Alt-Reg \
+        -annotate 0 $text \
+        $newImg
+        #-draw "text 0,0 'Gode minner'" \
+
+
+   
